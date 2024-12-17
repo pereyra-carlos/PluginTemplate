5cab05f0998360cda98ea121027f06494646da1d
Carlos Pereyra <Carlos.Pereyra.-ND@disney.com>
2024-12-17 00:02:29 -0300

    feat: first change

--- actions/SimpleAction/SimpleAction.py
+++ actions/SimpleAction/SimpleAction.py
@@ -16,21 +16,30 @@ gi.require_version("Adw", "1")
 from gi.repository import Gtk, Adw
 
 class SimpleAction(ActionBase):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
 
-        self.has_configuration = True
+    #     self.has_configuration = True
 
-        self.value = 0
-        self.key_down_time: int = 0
+    #     self.value = 0
+    #     self.key_down_time: int = 0
+
+    def __init__(self, action_id: str, action_name: str,
+                 deck_controller: DeckController, page: Page, coords: str, plugin_base: PluginBase):
+        super().__init__(action_id=action_id, action_name=action_name,
+            deck_controller=deck_controller, page=page, coords=coords, plugin_base=plugin_base)
+
+        backend_path = os.path.join(self.plugin_base.PATH, "actions", "SimpleAction", "backend", "backend.py") 
+        self.launch_backend(backend_path=backend_path, open_in_terminal=True)
         
     def on_ready(self) -> None:
-        settings = self.get_settings()
-        if settings.get("restore", True):
-            self.value = settings.get("value", 6)
+        # settings = self.get_settings()
+        # if settings.get("restore", True):
+        #     self.value = settings.get("value", 6)
 
-        self.show_value()
-        self.key_down_time: int = 0
+        # self.show_value()
+        # self.key_down_time: int = 0
+        self.set_center_label(str(self.backend.random_letter()))
         
     def on_key_down(self) -> None:
         self.set_center_label(str(self.value), font_size=30)
@@ -45,16 +54,17 @@ class SimpleAction(ActionBase):
         print("Key down")
     
     def on_key_up(self) -> None:
-        self.set_center_label(str(self.value), font_size=30)
-        randomLetter = random.choice(string.ascii_letters)
+        # self.set_center_label(str(self.value), font_size=30)
+        # randomLetter = random.choice(string.ascii_letters)
 
-        settings = self.get_settings()
-        settings["value"] = randomLetter
-        self.value = randomLetter
-        self.set_settings(settings)
-        self.show_value()
+        # settings = self.get_settings()
+        # settings["value"] = randomLetter
+        # self.value = randomLetter
+        # self.set_settings(settings)
+        # self.show_value()
 
-        print("Key down")
+        # print("Key down")
+        self.set_center_label("Pass:" + str(self.backend.random_letter()))
 
     def show_value(self) -> None:
         self.set_center_label(str(self.value), font_size=30)
