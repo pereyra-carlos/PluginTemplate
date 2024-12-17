a6e81d0943ea2d49a4351615638af2aa9477bd3a
Carlos Pereyra <Carlos.Pereyra.-ND@disney.com>
2024-12-17 00:39:41 -0300

    feat: first change

--- actions/SimpleAction/SimpleAction.py
+++ actions/SimpleAction/SimpleAction.py
@@ -25,19 +25,18 @@ class SimpleAction(ActionBase):
     #     self.value = 0
     #     self.key_down_time: int = 0
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        
-        print("launch backend")
-        backend_path = os.path.join(self.plugin_base.PATH, "actions", "SimpleAction", "backend", "backend.py") 
-        print("backend launched")
+    def __init__(self, action_id: str, action_name: str,
+                 deck_controller: DeckController, page: Page, coords: str, plugin_base: PluginBase):
+        super().__init__(action_id=action_id, action_name=action_name,
+            deck_controller=deck_controller, page=page, coords=coords, plugin_base=plugin_base)
 
-        self.launch_backend(backend_path=backend_path, open_in_terminal=False)
+        backend_path = os.path.join(self.plugin_base.PATH, "actions", "counter", "backend", "backend.py") 
+        self.launch_backend(backend_path=backend_path, open_in_terminal=True)
 
 
         
     def on_ready(self) -> None:
-        settings = self.get_settings()
+        #settings = self.get_settings()
         # if settings.get("restore", True):
         #     self.value = settings.get("value", 6)
 
@@ -54,45 +53,45 @@ class SimpleAction(ActionBase):
         # self.key_down_time: int = 0
         self.set_center_label(letter)
         
-    def on_key_down(self) -> None:
-        self.set_center_label(str(self.value), font_size=30)
-        randomLetter = random.choice(string.ascii_letters)
+    # def on_key_down(self) -> None:
+    #     self.set_center_label(str(self.value), font_size=30)
+    #     randomLetter = random.choice(string.ascii_letters)
 
-        settings = self.get_settings()
-        settings["value"] = randomLetter
-        self.set_settings(settings)
-        self.value = "carlos"
-        self.show_value()
+    #     settings = self.get_settings()
+    #     settings["value"] = randomLetter
+    #     self.set_settings(settings)
+    #     self.value = "carlos"
+    #     self.show_value()
 
-        print("Key down")
+    #     print("Key down")
     
-    def on_key_up(self) -> None:
-        # self.set_center_label(str(self.value), font_size=30)
-        # randomLetter = random.choice(string.ascii_letters)
+    # def on_key_up(self) -> None:
+    #     # self.set_center_label(str(self.value), font_size=30)
+    #     # randomLetter = random.choice(string.ascii_letters)
 
-        # settings = self.get_settings()
-        # settings["value"] = randomLetter
-        # self.value = randomLetter
-        # self.set_settings(settings)
-        # self.show_value()
+    #     # settings = self.get_settings()
+    #     # settings["value"] = randomLetter
+    #     # self.value = randomLetter
+    #     # self.set_settings(settings)
+    #     # self.show_value()
 
-        # print("Key down")
-        self.set_center_label(str(self.backend.get_letter()))
+    #     # print("Key down")
+    #     self.set_center_label(str(self.backend.get_letter()))
 
-    def show_value(self) -> None:
-        self.set_center_label(str(self.value), font_size=30)
+    # def show_value(self) -> None:
+    #     self.set_center_label(str(self.value), font_size=30)
 
-        settings = self.get_settings()
-        settings["value"] = self.value
-        self.set_settings(settings)
+    #     settings = self.get_settings()
+    #     settings["value"] = self.value
+    #     self.set_settings(settings)
 
-    def on_long_press(self):
-        settings = self.get_settings()
-        self.set_center_label(str(self.value), font_size=30)
-        randomLetter = random.choice(string.ascii_letters)
+    # def on_long_press(self):
+    #     settings = self.get_settings()
+    #     self.set_center_label(str(self.value), font_size=30)
+    #     randomLetter = random.choice(string.ascii_letters)
 
-        settings = self.get_settings()
-        settings["value"] = randomLetter
-        self.set_settings(settings)
+    #     settings = self.get_settings()
+    #     settings["value"] = randomLetter
+    #     self.set_settings(settings)
         
-        self.show_value()
+    #     self.show_value()
