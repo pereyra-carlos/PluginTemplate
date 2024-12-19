3891bfd52e16749d9e7c1064458946c331fc8d91
Carlos Pereyra <Carlos.Pereyra.-ND@disney.com>
2024-12-19 12:33:29 -0300

    feat: first change

--- backend/backend.py
+++ backend/backend.py
@@ -20,14 +20,15 @@ class Backend(BackendBase):
     def run_job_prod(self) -> str:
         #run_jenkins_job.ejecutar_job_en_jenkins()
         #return run_jenkins_job.ejecutar_job_en_jenkins()
-        
-        yield "a"
-        time.sleep(5)
-        yield "b"
         #return run_job_and_wait.ejecutar_job_completo
+        
+        # yield "a"
+        # time.sleep(5)
+        # yield "b"
     
-        #for estado in ejecutar_job_completo():
-        #    print(estado)
+        for estado in ejecutar_job_completo():
+            print(estado)
+            yield estado
     
 
     #def increase_count(self) -> None:
